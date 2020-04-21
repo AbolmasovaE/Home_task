ce9c0885027b425936e43bd48e3c6b8503966ca9
AbolmasovaE <64089190+AbolmasovaE@users.noreply.github.com>
2020-04-21 19:50:26 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Task_3
+{
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            double a, b;
+            char z;
+            Console.WriteLine("Введите первое число ");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите действие (+, -, *, /) ");
+            z = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine("Введите второе число ");
+            b = Convert.ToDouble(Console.ReadLine());
+            switch (z)
+            {
+                case '+':
+                    Console.WriteLine("{0}+{1}={2}", a, b, a + b);
+                    break;
+                case '-':
+                    Console.WriteLine("{0}-{1}={2}", a, b, a - b);
+                    break;
+                case '*':
+                    Console.WriteLine("{0}*{1}={2}", a, b, a * b);
+                    break;
+                case '/':
+                    Console.WriteLine("{0}/{1}={2}", a, b, a / b);
+                    break;
+                default:
+                    Console.WriteLine("Ошибка");
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
