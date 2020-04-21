cc2e90e5ca954117084ab19518214ce3a220f69c
AbolmasovaE <64089190+AbolmasovaE@users.noreply.github.com>
2020-04-21 19:52:52 +0300

    Add files via upload

---
+++ Program1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+            {
+                int count = 0;
+                for (int i = 2; i <= 88; i += 2)
+                {
+                    count += i;
+                }
+                Console.Write(count);
+            }
+        }
+    }
+
+
---
+++ Program2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Task_1
+{
+    class Program
+    {
+        public static int Main()
+        {
+            for (int i = 0; i <= 40; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.Write("{0} ", i);
+                }
+            }
+            Console.ReadKey();
+            return 0;
+        }
+    }
+}
+    
